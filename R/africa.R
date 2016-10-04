80808991120f8886bb64bbb5d068d50a8e661258
Sam <sboysel@gmail.com>
2016-10-03 18:38:13 -0700

    change data source and build process

--- R/africa.R
+++ R/africa.R
@@ -1,16 +1,72 @@
-#' Administrative Boundaries for 58 Countries within the African Continent
+#' Administrative Boundaries for Countries within the African Continent
+#'
+#' Source: \url{http://www.naturalearthdata.com/downloads/10m-cultural-vectors/}
 #'
 #' \describe{
-#' \item{ISO, ISO2, ISON}{}
-#' \item{NAME_*}{Country name in a number of various dialects}
-#' \item{VALIDFR, VALIDTO}{First / last year of nationhood}
-#' \item{POP2000}{Population in Year 2000}
-#' \item{SQKM}{Area in square kilometers}
-#' \item{POPSQKM}{Population Density = (POP2000 / SQKM)}
-#' \item{UNREGION1, UNREGION2}{}
-#' \item{DEVELOPING}{}
-#' \item{WBREGION}{World Bank Region}
-#' \item{WBINCOME}{World Bank Income Class}
-#' \item{WBDEBT}{World Bank Sovereign Debt Class}
+#'  \item{scalerank}{}
+#'  \item{featurecla}{}
+#'  \item{labelrank}{}
+#'  \item{sovereignt}{}
+#'  \item{sov_a3}{}
+#'  \item{adm0_dif}{}
+#'  \item{level}{}
+#'  \item{type}{}
+#'  \item{admin}{}
+#'  \item{adm0_a3}{}
+#'  \item{geou_dif}{}
+#'  \item{geounit}{}
+#'  \item{gu_a3}{}
+#'  \item{su_dif}{}
+#'  \item{subunit}{}
+#'  \item{su_a3}{}
+#'  \item{brk_diff}{}
+#'  \item{name}{}
+#'  \item{name_long}{}
+#'  \item{brk_a3}{}
+#'  \item{brk_name}{}
+#'  \item{brk_group}{}
+#'  \item{abbrev}{}
+#'  \item{postal}{}
+#'  \item{formal_en}{}
+#'  \item{formal_fr}{}
+#'  \item{note_adm0}{}
+#'  \item{note_brk}{}
+#'  \item{name_sort}{}
+#'  \item{name_alt}{}
+#'  \item{mapcolor7}{}
+#'  \item{mapcolor8}{}
+#'  \item{mapcolor9}{}
+#'  \item{mapcolor13}{}
+#'  \item{pop_est}{}
+#'  \item{gdp_md_est}{}
+#'  \item{pop_year}{}
+#'  \item{lastcensus}{}
+#'  \item{gdp_year}{}
+#'  \item{economy}{}
+#'  \item{income_grp}{}
+#'  \item{wikipedia}{}
+#'  \item{fips_10}{}
+#'  \item{iso_a2}{}
+#'  \item{iso_a3}{}
+#'  \item{iso_n3}{}
+#'  \item{un_a3}{}
+#'  \item{wb_a2}{}
+#'  \item{wb_a3}{}
+#'  \item{woe_id}{}
+#'  \item{adm0_a3_is}{}
+#'  \item{adm0_a3_us}{}
+#'  \item{adm0_a3_un}{}
+#'  \item{adm0_a3_wb}{}
+#'  \item{continent}{}
+#'  \item{region_un}{}
+#'  \item{subregion}{}
+#'  \item{region_wb}{}
+#'  \item{name_len}{}
+#'  \item{long_len}{}
+#'  \item{abbrev_len}{}
+#'  \item{tiny}{}
+#'  \item{homepart}{} 
 #' }
+#' 
+#' @import sp
 "africa"
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![Travis-CI Build Status](https://travis-ci.org/sboysel/africa.svg?branch=master)](https://travis-ci.org/sboysel/africa)
 
-Administrative boundaries for 58 African countries packaged for `R` as a `SpatialPolygonsDataFrame`.
+Administrative boundaries for African countries packaged for `R` as a `SpatialPolygonsDataFrame`.
 
 ## Usage
 ```{r}
@@ -12,7 +12,7 @@ library(africa)
 
 ## Source
 
-[GADM](http://www.gadm.org/)
+[Natural Earth](http://www.naturalearthdata.com/downloads/10m-cultural-vectors/)
 
 ## TODO
 
